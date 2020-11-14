5bd6c25b25be816fd9c57edb38b483910a5ad9a4
冬天的秘密 <yyimba@qq.com>
2020-11-14 22:12:27 +0800

    :sparkles: 插件支持发送图片消息

--- mqr-framework/src/main/java/com/molicloud/mqr/framework/util/MessageUtil.java
+++ mqr-framework/src/main/java/com/molicloud/mqr/framework/util/MessageUtil.java
@@ -4,6 +4,7 @@ import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.StrUtil;
 import com.molicloud.mqr.common.plugin.PluginResult;
 import com.molicloud.mqr.common.plugin.message.Ats;
+import com.molicloud.mqr.common.plugin.message.Img;
 import com.molicloud.mqr.common.plugin.message.Share;
 import lombok.experimental.UtilityClass;
 import net.mamoe.mirai.contact.ContactList;
@@ -11,11 +12,16 @@ import net.mamoe.mirai.contact.Friend;
 import net.mamoe.mirai.contact.Group;
 import net.mamoe.mirai.contact.Member;
 import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.message.data.Message;
+import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
 import net.mamoe.mirai.message.data.PlainText;
 import net.mamoe.mirai.message.data.ServiceMessage;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -59,7 +65,9 @@ public class MessageUtil {
     public Message convertFriendMessage(Object pluginResultData, Friend friend) {
         Message message = convertMessage(pluginResultData);
         if (message == null) {
-            // todo 待续
+            if (pluginResultData instanceof Img) {
+                return buildImageMessage(friend, ((Img) pluginResultData).getFileResource());
+            }
         }
         return message;
     }
@@ -78,21 +86,9 @@ public class MessageUtil {
         Message message = convertMessage(pluginResultData);
         if (message == null) {
             if (pluginResultData instanceof Ats) {
-                Ats ats = (Ats) pluginResultData;
-                List<String> mids = ats.getMids();
-                if (CollUtil.isNotEmpty(mids)) {
-                    ContactList<Member> memberContactList = group.getMembers();
-                    List<Member> memberList = mids.stream().map(mid -> memberContactList.get(Long.parseLong(mid))).collect(Collectors.toList());
-                    if (CollUtil.isNotEmpty(memberList)) {
-                        List<At> atList = memberList.stream().map(member -> new At(member)).collect(Collectors.toList());
-                        MessageChainBuilder messageChainBuilder = new MessageChainBuilder();
-                        atList.stream().forEach(at -> messageChainBuilder.append(at).append(" "));
-                        if (StrUtil.isNotBlank(ats.getContent())) {
-                            messageChainBuilder.append(ats.getContent());
-                        }
-                        return messageChainBuilder.build();
-                    }
-                }
+                buildGroupAtMessage(group, (Ats) pluginResultData);
+            } else if (pluginResultData instanceof Img) {
+                return buildImageMessage(group, ((Img) pluginResultData).getFileResource());
             }
         }
         return message;
@@ -106,5 +102,54 @@ public class MessageUtil {
      */
     private ServiceMessage buildShareMessage(Share share) {
         return ServiceMessage.Templates.share(share.getUrl(), share.getTitle(), share.getContent(), share.getCoverUrl());
+    }
+
+    /**
+     * 构建群「@」消息
+     *
+     * @param group
+     * @param ats
+     * @return
+     */
+    private MessageChain buildGroupAtMessage(Group group, Ats ats) {
+        List<String> mids = ats.getMids();
+        if (CollUtil.isNotEmpty(mids)) {
+            ContactList<Member> memberContactList = group.getMembers();
+            List<Member> memberList = mids.stream().map(mid -> memberContactList.get(Long.parseLong(mid))).collect(Collectors.toList());
+            if (CollUtil.isNotEmpty(memberList)) {
+                List<At> atList = memberList.stream().map(member -> new At(member)).collect(Collectors.toList());
+                MessageChainBuilder messageChainBuilder = new MessageChainBuilder();
+                atList.stream().forEach(at -> messageChainBuilder.append(at).append(" "));
+                if (StrUtil.isNotBlank(ats.getContent())) {
+                    messageChainBuilder.append(ats.getContent());
+                }
+                return messageChainBuilder.build();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 构建图片消息
+     *
+     * @param sender Group Or Friend 对象
+     * @param image
+     * @return
+     */
+    private Image buildImageMessage(Object sender, Object image) {
+        if (image instanceof File) {
+            return sender instanceof Group ?
+                    ((Group) sender).uploadImage((File) image) :
+                    ((Friend) sender).uploadImage((File) image);
+        } else if (image instanceof URL) {
+            return sender instanceof Group ?
+                    ((Group) sender).uploadImage((URL) image) :
+                    ((Friend) sender).uploadImage((URL) image);
+        } else if (image instanceof InputStream) {
+            return sender instanceof Group ?
+                    ((Group) sender).uploadImage((InputStream) image) :
+                    ((Friend) sender).uploadImage((InputStream) image);
+        }
+        return null;
     }
 }
