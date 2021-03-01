57ac1d7c0fdca9e39393c44aeb5927a18284452a
dlh <du__th@163.com>
2021-03-01 10:17:59 +0800

    :bug: 修复抖音视频插件bug

--- mqr-plugin/mqr-plugin-inner/mqr-plugin-dyurl/src/main/java/com/molicloud/mqr/plugin/dyurl/DyUrlPluginExecutor.java
+++ mqr-plugin/mqr-plugin-inner/mqr-plugin-dyurl/src/main/java/com/molicloud/mqr/plugin/dyurl/DyUrlPluginExecutor.java
@@ -1,5 +1,6 @@
 package com.molicloud.mqr.plugin.dyurl;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ReUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpRequest;
@@ -15,10 +16,10 @@ import com.molicloud.mqr.plugin.core.enums.RobotEventEnum;
 import com.molicloud.mqr.plugin.core.message.MessageBuild;
 import com.molicloud.mqr.plugin.core.message.make.Ats;
 import com.molicloud.mqr.plugin.core.message.make.Expression;
-import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+
 
 /**
  * @author dlh
@@ -29,9 +30,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class DyUrlPluginExecutor extends AbstractPluginExecutor {
 
-  private final String USER_AGENT = "Dalvik/2.1.0 (Linux; U; Android 10; COL-AL10 Build/HUAWEICOL-AL10)";
+  private final String USER_AGENT =
+      "Dalvik/2.1.0 (Linux; U; Android 10; COL-AL10 Build/HUAWEICOL-AL10)";
 
-  @PHook(name = "DyUrl",
+  @PHook(
+      name = "DyUrl",
       equalsKeywords = {"抖音视频解析"},
       robotEvents = {RobotEventEnum.FRIEND_MSG, RobotEventEnum.GROUP_MSG})
   public PluginResult messageHandler(PluginParam pluginParam) {
@@ -41,20 +44,29 @@ public class DyUrlPluginExecutor extends AbstractPluginExecutor {
       pluginResult.setHold(true);
       MessageBuild messageBuild = new MessageBuild();
       Ats ats = new Ats();
-      ats.setMids(Arrays.asList(pluginParam.getFrom()));
+      ats.setMids(CollUtil.toList(pluginParam.getFrom()));
       ats.setContent("请发送抖音视频地址");
       messageBuild.append(ats);
       messageBuild.append(new Expression(FaceDef.feiwen));
       pluginResult.setMessage(messageBuild);
     } else if (ExecuteTriggerEnum.HOLD.equals(pluginParam.getExecuteTriggerEnum())) {
-      String message = String.valueOf(pluginParam.getData());
+      String message = StrUtil.toString(pluginParam.getData());
       pluginResult.setHold(false);
-      pluginResult = getInfo(message, pluginResult);
+      MessageBuild messageBuild = new MessageBuild();
+      Ats ats = new Ats();
+      ats.setMids(CollUtil.toList(pluginParam.getFrom()));
+      ats.setContent(getInfo(message));
+      messageBuild.append(ats);
+      messageBuild.append(new Expression(FaceDef.xia));
+      pluginResult.setMessage(messageBuild);
     }
     return pluginResult;
   }
 
-  public PluginResult getInfo(String info, PluginResult pluginResult) {
+  public String getInfo(String info) {
+    if (!StrUtil.contains(info, "https://v.douyin.com")) {
+      return "请重试，并发送正确的抖音分享链接";
+    }
     String url = ReUtil.findAll("https://v.douyin.com/(.*?)/", info, 0).get(0);
     if (url != null) {
       String src = HttpRequest.get(url).header(HttpHeaders.USER_AGENT, USER_AGENT).execute().body();
@@ -62,25 +74,24 @@ public class DyUrlPluginExecutor extends AbstractPluginExecutor {
       if (id != null) {
         String jk = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=" + id;
         String json = HttpRequest.get(jk).execute().body();
-        String rel_url = ReUtil
-            .findAll("play_addr\":\\{\"uri\":\"(.*?)\",\"url_list\":\\[\"(.*?)\"", json, 2).get(0);
+        String rel_url =
+            ReUtil.findAll("play_addr\":\\{\"uri\":\"(.*?)\",\"url_list\":\\[\"(.*?)\"", json, 2)
+                .get(0);
         if (rel_url != null) {
           String video_url = rel_url.replaceAll("playwm", "play");
-          HttpResponse response = HttpRequest.get(video_url)
-              .header(HttpHeaders.USER_AGENT, USER_AGENT).execute();
+          HttpResponse response =
+              HttpRequest.get(video_url).header(HttpHeaders.USER_AGENT, USER_AGENT).execute();
           if (HttpStatus.HTTP_MOVED_TEMP == response.getStatus()) {
-            pluginResult.setMessage(
-                StrUtil.format("视频下载地址为：【{}】，请复制到浏览器进行下载", response.header(HttpHeaders.LOCATION)));
+            return StrUtil.format(
+                "视频下载地址为：【{}】，请复制到浏览器进行下载", response.header(HttpHeaders.LOCATION));
           }
         } else {
-          pluginResult.setMessage("获取真正的视频链接失败！");
+          return "获取真正的视频链接失败！";
         }
       } else {
-        pluginResult.setMessage("获取视频id失败！");
+        return "获取视频id失败！";
       }
-    } else {
-      pluginResult.setMessage("提取视频链接失败！");
     }
-    return pluginResult;
+    return "提取视频链接失败！";
   }
 }
